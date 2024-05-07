4c66a7f4da845105352f280392c620c328c99fd4
zacchen26 <166460046+zacchen26@users.noreply.github.com>
2024-05-06 22:54:14 -0700

    dasknfd

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from langchain.agents import create_csv_agent
+from langchain_experimental.agents import create_csv_agent
 from langchain.llms import OpenAI
 from dotenv import load_dotenv
 import os
