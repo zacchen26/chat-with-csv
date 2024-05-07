1ad5d45c4f889451d066968e863c6c3f13f2189d
zacchen26 <166460046+zacchen26@users.noreply.github.com>
2024-05-06 22:52:52 -0700

    jgyhyg

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
-from langchain.agents import create_csv_agent
+from langchain_experimental.agents import create_csv_agent
 from langchain.llms import OpenAI
 from dotenv import load_dotenv
 import os
 import streamlit as st
-
 
 def main():
     load_dotenv()
@@ -20,7 +19,6 @@ def main():
 
     csv_file = st.file_uploader("Upload a CSV file", type="csv")
     if csv_file is not None:
-
         agent = create_csv_agent(
             OpenAI(temperature=0), csv_file, verbose=True)
 
